a20c8b1d268191a4b32c8ac22f4db5cdaeb0f0c5
hosanamfd <134340493+hosanamfd@users.noreply.github.com>
2023-05-22 20:01:38 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+
+// tipos por valores
+let nome = 'rafael'; // isso é uma string literal- varias letras juntas em uma variavel
+let idade = 25; //number literal numeros juntos em uma variavel
+let estaAprovado = true; // bolean sim ou não
+let sobrenome = undefined; // undefined não é recomendado
+let corSelecionado = null; //redfinir um valor
+typeof nome; // comando para descobrir tipo de variavel
+ 
+// tipos por referencia
+//objetos
+let pessoa = {
+    nome: 'rafael',
+    idade: '27',
+    altura: '180',
+    sobrenome: 'de souza',
+    estadoCivil: 'casado',
+    possuiConta: true
+};
+//arrays - agrupa diferentes tipos de variaveis(objetos numeros boolean)
+let milena = [ milena, 17, tuiuti, ]
